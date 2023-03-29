4bcb6022e31c85c4e90834dcea1c6e535aeb8d9c
JazzyArmando1234 <jazzyarmando1234@gmail.com>
2023-03-29 16:41:45 +0300

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5501;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/res', (req, res) => {
-    res.send("ETSTESTETESTSETTTSTETETSETETSETSETSETSETTETESETSSETTETSETESTSE")
+    res.sendFile(__dirname + '/src/pannel.html');
   });
 
 app.listen(port, () => {
